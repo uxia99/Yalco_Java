b79e5e925d53f15fcfebd5bc491f68eff16a98b8
장유정 <306406j@gmail.com>
2023-10-01 03:01:08 +0900

    star II

--- src/sec04/chap04/star_practice.java
+++ src/sec04/chap04/star_practice.java
@@ -6,7 +6,7 @@ public class star_practice {
     public static void main(String[] args) {
         // 사각형 별찍기
         for (int i = 0; i < 5; i++) {
-            for(int j = 0; j < 6; j++) {
+            for(int j = 0; j < 5; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -51,30 +51,59 @@ public class star_practice {
 
         System.out.println("\n - - - - - - \n");
 
+        // 좌측 역 직각 삼각형
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n - - - - - - \n");
+
+        // 우측 역 직각 삼각형
+        for (int i = 0; i < 5; i++) {
+            // 공백 j
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // star j
+            for (int s = 5; s > i; s--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n - - - - - - \n");
+
+        // 역 피라미드
+        for (int i = 0; i < 5; i++) {
+            // 좌측 공백 j
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // star j
+            for (int j = i; j < 2 * 5 - i - 1; j++) {
+                System.out.print("*");
+            }
+            // 우측 공백 j
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n - - - - - - \n");
+
+        // 세로로 긴 삼각형(좌측)
+
+        System.out.println("\n - - - - - - \n");
+
+        // 세로로 긴 삼각형(우측)
+
+        System.out.println("\n - - - - - - \n");
+
         // 마름모
-//        System.out.println("홀수인 정수를 입력하시오");
-//        Scanner sc = new Scanner(System.in);
-//        int num = sc.nextInt();
-//        for (int i = 0; i < num; i++) {
-//            for (int j = 0; j < num; j++) {
-//                if (i < num / 2) { // 윗쪽 영역
-//                    if (i + j <= num / 2 - 1) // 왼쪽 위 공백
-//                        System.out.print(" ");
-//                    else if (j - i >= num / 2 + 1) // 오른쪽 위 공백
-//                        System.out.print(" ");
-//                    else
-//                        System.out.println("*");
-//            }
-//            else if (i > num / 2) { // 아랫쪽 영역
-//                    if (i - j >= num / 2 + 1) // 왼쪽 밑 공백
-//                        System.out.print(" ");
-//                    else if (i + j >= num / 2 * 3 + 1) // 오른쪽 밑 공백
-//                        System.out.print(" ");
-//                    else
-//                        System.out.print("*");
-//                }
-//            }
-//        }
-//            System.out.println();
+
     }
 }
