ed2b3f809dca2e76f02af4fb9ddf24cbad3ea9d8
장유정 <306406j@gmail.com>
2023-10-20 22:49:15 +0900

    Sec03.Chap05 Fir

--- src/sec03/chap05/ex01.java
+++ src/sec03/chap05/ex01.java
@@ -2,6 +2,14 @@ package sec03.chap05;
 
 public class ex01 {
     public static void main(String[] args) {
+        //  리터럴 방식
+        String str1 = "Hello World!";
+        String str2 = "안녕하세요 😆 반갑습니다~ 🖐️🖐️🖐️";
 
+        // 빈 문자열 가능
+        String str3 = "";
+
+        //  인스턴스 생성 방식
+        String str4 = new String("나중에 객체지향에서 자세히 배웁니다.");
     }
 }
--- src/sec03/chap05/ex04.java
+++ src/sec03/chap05/ex04.java
@@ -2,6 +2,24 @@ package sec03.chap05;
 
 public class ex04 {
     public static void main(String[] args) {
+        //  💡 타 자료형으로부터 문자열로 변환
+        String str1 = String.valueOf(true);
+        String str2 = String.valueOf(false);
+        String str3 = String.valueOf(123);
+        String str4 = String.valueOf(3.14f);
+        String str5 = String.valueOf('가');
+
+        String str6 = true + "";
+        String str7 = 123.45 + "";
+
+        String str123 = "123";
+
+        //  문자열을 정수 자료형으로 변환하기
+        byte bytNum = Byte.parseByte(str123);
+        short srtNum = Short.parseShort(str123);
+        int intNum = Integer.parseInt(str123);
+        long lngNum = Long.parseLong(str123);
+
 
     }
 }
