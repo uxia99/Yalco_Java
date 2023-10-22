2496d5a160878cc5881066b3f92175e4ca4bc8d6
장유정 <306406j@gmail.com>
2023-10-22 22:12:22 +0900

    Sec03.Chap05 Fin

--- src/sec03/chap05/ex04.java
+++ src/sec03/chap05/ex04.java
@@ -18,6 +18,25 @@ public class ex04 {
         byte bytNum = Byte.parseByte(str123);
         short srtNum = Short.parseShort(str123);
         int intNum = Integer.parseInt(str123);
-        long lngNum = Long.parseLong(str123)
+        long lngNum = Long.parseLong(str123);
+
+        double dblNum = Double.parseDouble("123.45");
+
+        //  대소문자 무관 'true'일 때 true 반환
+        boolean bool1 = Boolean.parseBoolean("TRUE");
+        boolean bool2 = Boolean.parseBoolean("true");
+        boolean bool3 = Boolean.parseBoolean("T");
+
+        String strA = "A";
+
+        //  다음 강에 배울 메소드
+        char chr = strA.charAt(0);
+
+        //  ⚠️ 런타임 에러 발생. 하나씩 주석해제해서 테스트
+        //  '실행'으로 테스트하거나, 디버그 후 콘솔 탭에서 확인해볼 것
+
+        //byte bytNum2 = Byte.parseByte("12345");
+        //int intNum2 = Integer.parseInt("123.45");
+        //double dblNum2 = Integer.parseInt("하나");
     }
 }
