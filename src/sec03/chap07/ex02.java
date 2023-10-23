a93f8b209601bcc64ab75936640da4eecbf1451d
장유정 <306406j@gmail.com>
2023-10-23 20:42:55 +0900

    Sec03.Chap06 Fir

--- src/sec03/chap07/ex02.java
+++ src/sec03/chap07/ex02.java
@@ -1,4 +1,7 @@
 package sec03.chap07;
 
 public class ex02 {
+    public static void main(String[] args) {
+        System.out.printf("%d, %5d, %05d%n- - - - -%n%n", 123, 123, 123);
+    }
 }
--- src/sec03/chap07/ex05.java
+++ src/sec03/chap07/ex05.java
@@ -1,4 +1,7 @@
 package sec03.chap07;
 
 public class ex05 {
+    public static void main(String[] args) {
+
+    }
 }
