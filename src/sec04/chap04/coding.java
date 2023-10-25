5c68bd59a60ee5c3f39940df951443e1f664cd7d
장유정 <306406j@gmail.com>
2023-10-25 17:07:39 +0900

    Sec04.Chap04 Fou

--- src/sec04/chap04/coding.java
+++ src/sec04/chap04/coding.java
@@ -62,5 +62,31 @@ public class coding {
 
             System.out.println(car);
         }
+
+        System.out.println("\n - - - - - - \n");
+
+        int enemies = 0;
+
+        System.out.println("일단 사격");
+
+        do {
+            System.out.println("탕");
+            if (enemies > 0) enemies--;
+        } while (enemies > 0);
+
+        System.out.println("사격중지 아군이다");
+
+        System.out.println("\n - - - - - - \n");
+
+        int x = 1;
+        int y = x;
+
+        while (x < 10) {
+            System.out.println("while 문 : " + x++);
+        }
+
+        do {
+            System.out.println("do ... while 문 : " + y++);
+        } while (y < 10);
     }
 }
