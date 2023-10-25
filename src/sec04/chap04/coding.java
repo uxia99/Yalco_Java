fc2a84b0c5ba4bf7c10a1acb72887969648629d6
장유정 <306406j@gmail.com>
2023-10-25 16:04:23 +0900

    Sec04.Chap04 Sec

--- src/sec04/chap04/coding.java
+++ src/sec04/chap04/coding.java
@@ -1,4 +1,31 @@
 package sec04.chap04;
 
 public class coding {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("\n - - - - - - \n");
+
+        int i = 0;
+
+        while (i < 10) {
+            System.out.println(i++);
+        }
+
+        System.out.println("\n - - - - - - \n");
+
+        double popInBillion = 7.837;
+
+        while (true) {
+            System.out.println("세계인구 : " + (popInBillion -= 0.1));
+            if (popInBillion <= 0) break;
+
+            System.out.println("인간의 욕심은 끝이 없고,");
+            System.out.println("같은 실수를 반복한다.");
+        }
+
+        System.out.println("인류 멸종");
+    }
 }
