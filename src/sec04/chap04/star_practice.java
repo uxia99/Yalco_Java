511b29dfc5c8d13b1b7e6a86b5d1080930aafb1e
장유정 <306406j@gmail.com>
2023-10-26 03:21:55 +0900

    Star IV

--- src/sec04/chap04/star_practice.java
+++ src/sec04/chap04/star_practice.java
@@ -102,7 +102,7 @@ public class star_practice {
             }
             System.out.println();
         }
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i <= 4; i++) {
             for (int j = 4; j >= i; j--) {
                 System.out.print("*");
             }
@@ -112,10 +112,100 @@ public class star_practice {
         System.out.println("\n - - - - - - \n");
 
         // 세로로 긴 삼각형(우측)
+        for(int i = 0; i < 5; i++) {
+            String stars = "";
+            // 증가
+            for(int j = 0; j < 5 - i; j++) {
+                stars += " ";
+            }
+            for(int j = 0; j < i; j++) {
+                stars += "*";
+            }
+            System.out.println(stars);
+        }
+
+        for(int i = 0; i < 5; i++) {
+            String stars = "";
+            // 감소
+            for(int j = 0; j < i; j++) {
+                stars += " ";
+            }
+            for(int j = 0; j < 5 - i; j++) {
+                stars += "*";
+            }
+            System.out.println(stars);
+        }
+
 
         System.out.println("\n - - - - - - \n");
 
         // 마름모
+        for(int i = 1; i <= 5; i++) {
+            // 왼쪽 공백
+            for(int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            // 별 개수
+            for(int k = 1; k <= 2*i-1 ; k++) {
+                System.out.print("*");
+            }
+            // 오른쪽 공백
+            for(int l = 5; l > i; l--) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+
+        for(int i = 1; i < 5; i++) {
+            // 왼쪽 공백
+            for(int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            // 별 수
+            for(int k = i; k < 2 * 5 - i - 1; k++) {
+                System.out.print("*");
+            }
+            // 오른쪽 공백
+            for(int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("\n - - - - - - \n");
+
+        // 마름모
+        for(int i = 0; i < 5; i++) {
+            // 왼쪽 공백
+            for(int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            // 별 개수
+            for(int k = 1; k <= 2 * i - 1 ; k++) {
+                System.out.print("*");
+            }
+            // 오른쪽 공백
+            for(int l = 5; l > i; l--) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+
+        for(int i = 0; i < 5; i++) {
+            // 왼쪽 공백
+            for(int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // 별 수
+            for(int k = i; k < 2 * 5 - i - 1; k++) {
+                System.out.print("*");
+            }
+            // 오른쪽 공백
+            for(int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
 
     }
 }
