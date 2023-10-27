4f074ea29d25e074bcbd570db2c4c238c09a92f4
장유정 <306406j@gmail.com>
2023-10-28 00:52:26 +0900

    Sec04.Chap06 Sev

--- src/sec04/chap06/ex03.java
+++ src/sec04/chap06/ex03.java
@@ -7,6 +7,14 @@ public class ex03 {
         upTo5(2);
         upTo5(4);
 
+        System.out.println("\n - - - - - - \n");
+
+        int fact1 = factorial(1);
+        int fact2 = factorial(2);
+        int fact3 = factorial(3);
+        int fact4 = factorial(4);
+        int fact5 = factorial(5);
+
     }
 
     static void upTo5 (int start) {
@@ -16,6 +24,10 @@ public class ex03 {
         } else {
             System.out.println("-- 종료 --");
         }
+    }
+
+    static int factorial (int num) {
+        return num == 0 ? 1 : num * factorial(--num);
     }
 
 }
