58878a46a582375ef78a7ce6268d893e74f8796b
장유정 <306406j@gmail.com>
2023-10-27 18:40:56 +0900

    Sec04.Chap05 Sec

--- src/sec04/chap05/coding.java
+++ src/sec04/chap05/coding.java
@@ -10,6 +10,29 @@ public class coding {
 
         x = 13; y = 9;
         arithmetic(x, y);
+
+
+        System.out.println("\n - - - - - - \n");
+
+
+        int int1 = add(2, 3);
+        System.out.println(int1);
+
+        System.out.println(add(4, 5));
+
+        int int2 = add(add(6, 7), add(8, 9));
+        System.out.println(int2);
+
+
+        System.out.println("\n - - - - - - \n");
+
+
+        System.out.println(
+                checkIfContain(
+                        "Hello World",
+                        "hello"
+                ) ? "Contain" : "Not Contain"
+        );
     }
 
     static void arithmetic (double a, double b) {
@@ -18,4 +41,13 @@ public class coding {
         System.out.printf("%f * %f = %f%n", a, b, a * b);
         System.out.printf("%f / %f = %f%n", a, b, a / b);
     }
+
+    static int add (int num1, int num2) {
+        return num1 + num2;
+    }
+
+    static boolean checkIfContain (String text, String token) {
+        return text.toLowerCase() .contains(token.toLowerCase());
+    }
+
 }
