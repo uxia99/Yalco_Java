c997ffa0246d8e61e03bfffc8531f6d26a00f8ca
장유정 <306406j@gmail.com>
2023-10-31 17:53:39 +0900

    Sec05.Chap03.ex02

--- src/sec05/chap03/ex01/YalcoChicken.java
+++ src/sec05/chap03/ex01/YalcoChicken.java
@@ -3,11 +3,12 @@ package sec05.chap03.ex01;
 public class YalcoChicken {
     //  ⭐️ 클래스/정적 필드와 메소드들 : 본사의 정보와 기능
     //  인스턴스마다 따로 갖고 있을 필요가 없는 것들에 사용
-    static String brand = "Yalco Chicken";
+    static String brand = "얄코치킨";
     static String contact () {
         //  ⚠️ 정적 메소드에서는 인스턴스 프로퍼티 사용 불가
         //  System.out.println(name);
-        return "%s입니다. 무엇을 도와드릴까요?".formatted(brand);
+        return "%s입니다. 무엇을 도와드릴까요?"
+                .formatted(brand);
     }
 
     int no;
