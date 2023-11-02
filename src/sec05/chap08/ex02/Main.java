b572a7bcbc2352de17c3b2f7dac3fc6f8bef6209
장유정 <306406j@gmail.com>
2023-11-02 17:22:50 +0900

    Sec05.Chap08.ex02

--- src/sec05/chap08/ex02/Main.java
+++ src/sec05/chap08/ex02/Main.java
@@ -1,4 +1,7 @@
 package sec05.chap08.ex02;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- src/sec05/chap08/ex02/Switch.java
+++ src/sec05/chap08/ex02/Switch.java
@@ -1,4 +1,17 @@
 package sec05.chap08.ex02;
 
-public class Switch {
+public class Switch extends FormElement {
+    private boolean on;
+
+    public Switch(int space, boolean on) {
+        super(space);
+        this.on = on;
+    }
+
+    @Override
+    void func() {
+        on = !on;
+        System.out.println((on ? "ON" : "OFF") + "으로 전환");
+    }
 }
+
