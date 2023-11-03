2fb41da32b7aac48c3e4ff582e870f1587432397
장유정 <306406j@gmail.com>
2023-11-03 18:24:04 +0900

    Sec05.Chap09.ex01

---
+++ src/sec05/chap09/ex01/Mammal.java
@@ -0,0 +1,9 @@
+package sec05.chap09.ex01;
+
+public abstract class Mammal { // 포유 동물
+    public boolean hibernation;
+
+    public Mammal(boolean hibernation) {
+        this.hibernation = hibernation;
+    }
+}
---
+++ src/sec05/chap09/ex01/Reptile.java
@@ -0,0 +1,7 @@
+package sec05.chap09.ex01;
+
+public abstract class Reptile { // 파충류
+    public boolean isColdBlooded () {
+        return true;
+    }
+}
