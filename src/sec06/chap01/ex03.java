fb29c7b80b5813800cfbec92bd5010c55c051347
장유정 <306406j@gmail.com>
2023-11-03 23:39:22 +0900

    Sec06.Chap01 Fin

---
+++ src/sec06/chap01/ex03.java
@@ -0,0 +1,12 @@
+package sec06.chap01;
+
+public class ex03 {
+//    String str = "바깥쪽";
+//    {
+//        String str = "안쪽"; // ⚠️ 불가
+//    }
+
+//        while (true) {
+//        String str = "안쪽"; // ⚠️ 불가
+//    }
+}
