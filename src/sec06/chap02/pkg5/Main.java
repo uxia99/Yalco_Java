f90e1436750624bc426a1b563226002b8a593d91
장유정 <306406j@gmail.com>
2023-11-05 03:31:27 +0900

    Sec06.Chap02.pkg5 Fir

---
+++ src/sec06/chap02/pkg5/Main.java
@@ -0,0 +1,20 @@
+package sec06.chap02.pkg5;
+
+public class Main {
+    //  java.lang 패키지에 속한 기본 라이브러리 클래스들
+        System.out.println("이 클래스들은 왜 임포트를 안 해도 될까요?");
+    Object object = new Object();
+    String str = new String("java.lang 패키지 소속이라 그럼");
+    Integer integer;
+    Math math;
+
+    //  다른 패키지에 속한 기본 라이브러리 클래스들
+    ArrayList arrayList;
+    Calendar calendar;
+    Optional optional;
+    Iterator iterator;
+    InputStream inputStream;
+    Serializable serializable;
+    Stream stream;
+    Button button;
+}
