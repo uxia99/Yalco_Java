befa3acb09fc63986a87260f741ddf65b0a3c932
장유정 <306406j@gmail.com>
2023-11-04 20:11:47 +0900

    Sec06.Chap02.pkg1 Fou

--- src/sec06/chap02/pkg1/Friend.java
+++ src/sec06/chap02/pkg1/Friend.java
@@ -5,7 +5,7 @@ public class Friend {
     Parent parent = new Parent();
 
     // int aa = new Parent().a; // ⚠️ 불가
-    int bb = parent.b;
+    int bb = parent.b; // 자식클래스가 아니기 때문에 그냥 가져다쓰지는 못하고, 인스턴스를 만든 후 끌어다 사용 가능
     int cc = parent.c; // 💡 protected - 같은 패키지, 비 상속관계
     int dd = parent.d;
 }
