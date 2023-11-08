0c05b99d2c221fc4937513996c825b7afb619c86
장유정 <306406j@gmail.com>
2023-11-09 04:22:12 +0900

    Sec06.Chap05.ex02

---
+++ src/sec06/chap05/ex02.java
@@ -0,0 +1,10 @@
+package sec06.chap05;
+
+public class ex02 {
+    public static void main(String[] args) {
+
+        for (var arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
