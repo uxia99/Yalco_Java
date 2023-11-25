d1e2e99335603ee62747387e43bb01fd381ac077
장유정 <306406j@gmail.com>
2023-11-25 19:18:26 +0900

    Sec06.Chap07.ex02 Thi

--- src/sec06/chap07/ex02/Main.java
+++ src/sec06/chap07/ex02/Main.java
@@ -13,5 +13,16 @@ public class Main {
         for (var click : new Button.ClickInfo [] {click1, click2, click3}) {
             click.printInfo();
         }
+
+        System.out.println("\\n- - - - -\\n");
+
+        Button.ClickInfo click4 = button.func(111, 222, Button.ClickedBy.LEFT);
+        Button.ClickInfo click5 = button.func(111, 222, Button.ClickedBy.LEFT);
+
+        //  ⭐️  레코드 역시 참조형
+        //  내용이 같은지 여부는 equals 메소드로 확인
+        boolean click4n5Same = click4 == click5;
+        boolean click4n5Equal = click4.equals(click5);
+        boolean click4n1Equal = click4.equals(click1);
     }
 }
