a782eab742d1543ee9bfd60bd3e5d2293a55e0e3
장유정 <306406j@gmail.com>
2023-11-25 19:09:36 +0900

    Sec06.Chap07.ex02 Sec

--- src/sec06/chap07/ex02/Main.java
+++ src/sec06/chap07/ex02/Main.java
@@ -1,5 +1,17 @@
 package sec06.chap07.ex02;
 
-public class Main {
-}
+import static sec06.chap07.ex02.Button.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Button button = new Button();
+
+        Button.ClickInfo click1 = button.func(123, 456, ClickedBy.LEFT);
+        Button.ClickInfo click2 = button.func(492, 97, ClickedBy.LEFT);
+        Button.ClickInfo click3 = button.func(12, 36, ClickedBy.RIGHT);
+
+        for (var click : new Button.ClickInfo [] {click1, click2, click3}) {
+            click.printInfo();
+        }
+    }
+}
