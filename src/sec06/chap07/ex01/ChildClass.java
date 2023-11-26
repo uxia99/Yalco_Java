68f2a83be30f27ae56a1f9a9a25db58d46eb790d
장유정 <306406j@gmail.com>
2023-11-26 20:46:30 +0900

    Sec06.Chap07.ex01 Sec

--- src/sec06/chap07/ex01/ChildClass.java
+++ src/sec06/chap07/ex01/ChildClass.java
@@ -1,4 +1,23 @@
 package sec06.chap07.ex01;
 
 public class ChildClass {
+    private final String name;
+    private final int birthYear;
+    private final Gender gender;
+
+    public ChildClass(String name, int birthYear, Gender gender) {
+        this.name = name;
+        this.birthYear = birthYear;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getBirthYear() {
+        return birthYear;
+    }
+    public Gender getGender() {
+        return gender;
+    }
 }
