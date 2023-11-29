ed2a2ba0caf19ac8c6a013aba470abb7a8224d71
장유정 <306406j@gmail.com>
2023-11-29 17:33:43 +0900

    Sec06.Chap07.ex01 Fou

--- src/sec06/chap07/ex01/Gender.java
+++ src/sec06/chap07/ex01/Gender.java
@@ -4,10 +4,6 @@ public enum Gender {
     MALE("👦🏻"), FEMALE("👧🏼");
 
     private String emoji;
-    Gender(String emoji) {
-        this.emoji = emoji;
-    }
-    public String getEmoji() {
-        return emoji;
-    }
+    Gender(String emoji) { this.emoji = emoji; }
+    public String getEmoji() { return emoji; }
 }
--- src/sec06/chap07/ex01/Main.java
+++ src/sec06/chap07/ex01/Main.java
@@ -1,5 +1,26 @@
 package sec06.chap07.ex01;
 
 public class Main {
+    public static void main(String[] args) {
+        Child child1 = new Child("홍길동", 2020, Gender.MALE);
+        //  💡 toString 메소드 구현 (이후 배울 Object에서 상속받아 오버라이드)
+        String childStr = child1.toString();
+
+        var children = new Child[] {
+                new Child("장유정", 1999, Gender.FEMALE),
+                new Child("김재헌", 1997, Gender.MALE),
+                new Child("장우진", 2003, Gender.MALE),
+                new Child("김은서", 1999, Gender.FEMALE),
+        };
+
+        for (var child : children) {
+            System.out.printf(
+                    "%s %d년생 %s 어린이%n",
+                    child.gender().getEmoji(),
+                    child.birthYear(),
+                    child.name()
+            );
+        }
+    }
 }
 
