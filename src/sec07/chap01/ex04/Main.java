e110164a9fe1d3138d24a24c63a00d9b8d816c7e
장유정 <306406j@gmail.com>
2023-12-04 16:54:20 +0900

    Sec07.Chap01.ex04 Sec

--- src/sec07/chap01/ex04/Main.java
+++ src/sec07/chap01/ex04/Main.java
@@ -1,5 +1,20 @@
 package sec07.chap01.ex04;
 
 public class Main {
-}
+    public static void main(String[] args) {
+        NotCloneable notCloneable = new NotCloneable(
+                "클릭들 1", 1, new int[] {1, 2, 3},
+                new Click(12, 34),
+                new Click[] { new Click(12, 34), new Click(56, 78) }
+        );
 
+        NotCloneable clone1 = null;
+
+        try { // ❓ try 문 : 오류에 대비하기 섹션에서 배울 것
+            clone1 = (NotCloneable) notCloneable.clone();
+        } catch (CloneNotSupportedException e) {
+            System.out.printf("⚠ 복제중 오류 발생 : %s%n", notCloneable);
+        }
+        //  ⚠️ 복사 실패 - CloneNotSupportedException 이라는 오류 발생
+    }
+}
