67fb9474bb948640bd99d69a9aea26a32cb1ad85
장유정 <306406j@gmail.com>
2023-12-05 17:57:22 +0900

    Sec07.Chap02.ex02 Sec

--- src/sec07/chap02/Ex02.java
+++ src/sec07/chap02/Ex02.java
@@ -16,5 +16,26 @@ public class Ex02 {
 
         Boolean blnInst1 = Boolean.valueOf(true);
         boolean blnPrim1 = blnInst1.booleanValue();
+
+
+        System.out.println("\n- - - - - -\n");
+
+
+        // static int add(Integer a, Integer b) { return a + b; }
+
+        //  💡 오토박싱
+        Integer intInst2 = 234;
+        Double dblInst2 = 1.414213;
+
+        //  💡 오토언박싱
+        char chrPrim2 = Character.valueOf('B');
+        boolean blnPrim2 = Boolean.valueOf(false);
+
+        //  원시값과 래핑 클래스 인스턴스끼리의 연산
+        int intPrim2 = intPrim1 + intInst2;
+        Integer intInst3 = intPrim2 + intInst2;
+
+        //  메소드 등 사용처들에 혼용 가능
+        Integer intInst4 = add(3, 5);
     }
 }
---
+++ src/sec07/chap02/Ex03.java
@@ -0,0 +1,7 @@
+package sec07.chap02;
+
+public class Ex03 {
+    public static void main(String[] args) {
+
+    }
+}
