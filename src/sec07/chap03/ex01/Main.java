001d28f917db01a5b7fb157868b77a89c6f01743
장유정 <306406j@gmail.com>
2023-12-06 01:36:22 +0900

    Sec07.Chap03.Ex01 Sec

--- src/sec07/chap03/ex01/Main.java
+++ src/sec07/chap03/ex01/Main.java
@@ -1,8 +1,21 @@
 package sec07.chap03.ex01;
 
+import sec05.chap08.ex01.YalcoChicken;
+
 public class Main {
     public static void main(String[] args) {
+        int randNum = pickRandom(123, 456);
+        boolean randBool = pickRandom(true, false);
+        String randStr = pickRandom("마루치", "아라치");
 
+        //  import sec05.chap08.ex01.YalcoChicken;
+        YalcoChicken store1 = new YalcoChicken("판교");
+        YalcoChicken store2 = new YalcoChicken("역삼");
+        YalcoChicken randStore = pickRandom(store1, store2);
+
+        //  ⚠️ 타입이 일관되지 않고 묵시적 변환 불가하면 오류
+        //  double randFlt = pickRandom("hello", "world");
+        double randDbl = pickRandom(12, 34);
     }
 
     //  제네릭 메소드
