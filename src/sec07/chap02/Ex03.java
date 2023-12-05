7191497dc527f3f868b81257ab0cdb1c77da5ef5
장유정 <306406j@gmail.com>
2023-12-05 18:00:47 +0900

    Sec07.Chap02.Ex03 Fir

--- src/sec07/chap02/Ex03.java
+++ src/sec07/chap02/Ex03.java
@@ -2,6 +2,24 @@ package sec07.chap02;
 
 public class Ex03 {
     public static void main(String[] args) {
+        //  💡 숫자 클래스 메소드들
 
+        //  CharSequence로부터 인스턴스 반환
+        //  ⭐ CharSequence : String 등이 구현하는 인터페이스
+        Integer int1 = Integer.valueOf("123"); // 문자열로부터 인스턴스 반환
+
+        //  CharSequence로부터 원시값 반환
+        //  💡 다른 숫자, 불리언 래퍼 자료형들에도 존재 (parseDouble, parseBoolean...)
+        int int2 = Integer.parseInt("123"); // 원시값 반환
+
+        //  parseInt(CharSequence, 진수)
+        //  정수 자료형들에만 존재
+        //  ⭐ CharSequence : String 등이 구현하는 인터페이스
+        int int_123_oct = Integer.parseInt("123", 8);
+        int int_123_dec = Integer.parseInt("123", 10);
+        int int_123_hex = Integer.parseInt("123", 16);
+
+        //  parseInt(CharSequence, 시작위치, 끝위치, 진수)
+        int int3 = Integer.parseInt("1234567", 3, 5, 10);
     }
 }
